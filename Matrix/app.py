c0b43c9bb73b7ce25a8ebc9f34fcfcfed94d3370
Vaishnavi Pulate <you@example.com>
2026-07-28 11:05:25 +0530

    App.py corrected

--- Matrix/app.py
+++ Matrix/app.py
@@ -2,10 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return "<h1>Hello Python from Flask running inside Docker!</h1>"
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
     
